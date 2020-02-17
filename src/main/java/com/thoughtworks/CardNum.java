772b862196a5584deff8b91387c57dea835b19e3
Ryan.Wang <ranwang1995@outlook.com>
2020-02-18 02:46:58 +0800

    [WangRan] feat: add enum CardNum

---
+++ src/main/java/com/thoughtworks/CardNum.java
@@ -0,0 +1,28 @@
+package com.thoughtworks;
+
+public enum CardNum {
+    ACE("A"),
+    ONE("1"),
+    TWO("2"),
+    THREE("3"),
+    FOUR("4"),
+    FIVE("5"),
+    SIX("6"),
+    SEVEN("7"),
+    EIGHT("8"),
+    NINE("9"),
+    TEN("10"),
+    JACK("J"),
+    QUEEN("Q"),
+    KING("K");
+
+    private String symbol;
+
+    CardNum(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+}
